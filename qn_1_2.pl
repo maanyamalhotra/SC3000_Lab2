79765f00c505bba0a89a6a2db741e3108d7eb960
Saba <saba.x.azad@gmail.com>
2026-04-11 23:46:18 +0800

    added my contribution

---
+++ qn_1_2.pl
@@ -0,0 +1,11 @@
+% facts
+competitor(sumsum, appy).
+smart_phone_technology(galacticas3).
+developed(sumsum, galacticas3).
+stole(stevey, galacticas3).
+boss(stevey, appy).
+
+% rules
+rival(X, Y) :- competitor(X, Y).
+business(X) :- smart_phone_technology(X).
+unethical(X) :- boss(X, Y), stole(X, Z), business(Z), developed(W, Z), rival(W, Y).
