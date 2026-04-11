454b637ae9333f1d800ddb1a85e44671b9ce9cbd
Saba <saba.x.azad@gmail.com>
2026-04-12 00:01:08 +0800

    Fix formatting of comments and rules in qn_1_2.pl

--- qn_1_2.pl
+++ qn_1_2.pl
@@ -1,11 +1,11 @@
-% facts
+% 5 ground facts
 competitor(sumsum, appy).
 smart_phone_technology(galacticas3).
 developed(sumsum, galacticas3).
 stole(stevey, galacticas3).
 boss(stevey, appy).
 
-% rules
+% 3 rules
 rival(X, Y) :- competitor(X, Y).
 business(X) :- smart_phone_technology(X).
-unethical(X) :- boss(X, Y), stole(X, Z), business(Z), developed(W, Z), rival(W, Y).
+unethical(X) :- boss(X, Y), stole(X, Z), business(Z), developed(W, Z), rival(W,Y).
